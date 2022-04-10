738cf806b960ed3603d7e3b3e738bdd4e9baf77f
Gerardo Andree Pérez Cabrera <88795594+gperez2018345@users.noreply.github.com>
2022-04-09 23:25:32 -0600

    final

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="headerContainer">
         <h1> LevelUp Pool </h1>
-        <p> Desarrollador React</p>
+        <p> Desarrollador React | Gerardo Perez</p>
         <Link to="/about">
           <button> Recursos </button>
         </Link>
